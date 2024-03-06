29201ee9cc46330bcdeacbe20efcd599cbd61cae
Ross Cohen <rosscohen1990@gmail.com>
2024-03-06 04:51:27 -0500

    Update brand.tsx

--- components/ui/brand.tsx
+++ components/ui/brand.tsx
@@ -12,15 +12,13 @@ export const Brand: FC<BrandProps> = ({ theme = "dark" }) => {
   return (
     <Link
       className="flex cursor-pointer flex-col items-center hover:opacity-50"
-      href="https://www.chatbotui.com"
+      href="https://myapps.ai"
       target="_blank"
       rel="noopener noreferrer"
     >
-      <div className="mb-2">
-        <ChatbotUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
-      </div>
 
-      <div className="text-4xl font-bold tracking-wide">Chatbot UI</div>
+
+      <div className="text-4xl font-bold tracking-wide">AI Tutor Premium</div>
     </Link>
   )
 }
