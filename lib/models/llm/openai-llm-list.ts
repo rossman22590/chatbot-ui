431159e4d25c64a06a40d37df2ceb3d912f292f7
Ross Cohen <rosscohen1990@gmail.com>
2024-03-14 13:51:40 -0400

    Update openai-llm-list.ts

--- lib/models/llm/openai-llm-list.ts
+++ lib/models/llm/openai-llm-list.ts
@@ -24,15 +24,15 @@ const GPT4Vision: LLM = {
   imageInput: true
 }
 
-// GPT-4 (UPDATED 1/29/24)
-const GPT4: LLM = {
-  modelId: "gpt-4",
-  modelName: "GPT-4",
-  provider: "openai",
-  hostedId: "gpt-4",
-  platformLink: OPENAI_PLATORM_LINK,
-  imageInput: false
-}
+// // GPT-4 (UPDATED 1/29/24)
+// const GPT4: LLM = {
+//   modelId: "gpt-4",
+//   modelName: "GPT-4",
+//   provider: "openai",
+//   hostedId: "gpt-4",
+//   platformLink: OPENAI_PLATORM_LINK,
+//   imageInput: false
+// }
 
 // GPT-3.5 Turbo (UPDATED 1/25/24)
 const GPT3_5Turbo: LLM = {
@@ -44,4 +44,6 @@ const GPT3_5Turbo: LLM = {
   imageInput: false
 }
 
-export const OPENAI_LLM_LIST: LLM[] = [GPT4Turbo, GPT4Vision, GPT4, GPT3_5Turbo]
+export const OPENAI_LLM_LIST: LLM[] = [GPT4Turbo, GPT4Vision, GPT3_5Turbo]
+
+ // GPT4,
